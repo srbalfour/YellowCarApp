1cc510b44e89a5952de42b69bd01a213844a0b6b
srbalfour <44952394+srbalfour@users.noreply.github.com>
2023-05-25 14:17:54 +0100

    Added topbar
    Installed TypeScript and Added topbar

---
+++ App.tsx
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import TopBarComponent from './TopBar';
+
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <View style={styles.container}>
+
+        <TopBarComponent></TopBarComponent>
+
+
+        <Text>Open up App.js to start working on your app!</Text>
+
+
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ TopBar.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Appbar } from 'react-native-paper';
+
+
+const TopBarComponent = () => (
+    <Appbar.Header>
+        <Appbar.BackAction onPress={() => { }} />
+        <Appbar.Content title="Title" />
+        <Appbar.Action icon="calendar" onPress={() => { }} />
+        <Appbar.Action icon="magnify" onPress={() => { }} />
+    </Appbar.Header>
+);
+
+export default TopBarComponent;
