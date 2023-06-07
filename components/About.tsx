3bce9ca26149eb51515d93e2b548bdee6595f4e1
srbalfour <44952394+srbalfour@users.noreply.github.com>
2023-06-07 15:49:59 +0100

    Added about section
    Added about section to top bar.

--- components/About.tsx
+++ components/About.tsx
@@ -16,20 +16,28 @@ const About = ({visible, onClose}: Props) => {
 
         <View>
             <Portal>
-                <Dialog visible={visible} onDismiss={onClose} >
+                <Dialog visible={visible} onDismiss={onClose} style={aboutStyles.dialog}>
                     <Dialog.Title style={{ color: colours.textPrimary }}>About</Dialog.Title>
 
+                    <Dialog.Content>
+                        <Text style={{ fontSize:10, color: colours.textSecondary }}>App by Shanna Balfour, 2023. Visit https://github.com/srbalfour/YellowCarApp to see more.</Text>
+                    </Dialog.Content>
+
                     <Dialog.Actions>
-                        <Button onPress={onClose}>Cancel</Button>
-                        <Button onPress={() => {
-                            
-                            onClose();
-                        }}>Add</Button>
+                        <Button onPress={() => {onClose();}} textColor={colours.textPrimary}>Close</Button>
                     </Dialog.Actions>
                 </Dialog>
             </Portal>
         </View>
     )
 };
+
+const aboutStyles = StyleSheet.create({
+    dialog: {
+        backgroundColor: colours.backgroundMain,
+
+    },
+
+});
 
 export default About;
