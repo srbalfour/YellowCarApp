6a7ad96bd3c6b7293826d2f10d24d76af6f02534
srbalfour <44952394+srbalfour@users.noreply.github.com>
2023-06-12 14:35:42 +0100

    Added alert
    Alert the user to enter a username when the name is left empty

--- components/NewPlayer.tsx
+++ components/NewPlayer.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { Alert, View } from 'react-native';
 import { Button, Dialog, Portal, PaperProvider, Text } from 'react-native-paper';
 import { SafeAreaView, StyleSheet, TextInput } from 'react-native';
 import colours from '../constants/colours';
@@ -20,51 +20,47 @@ interface Props {
 const NewPlayer = ({ visible, setPlayers, onClose, onClear }: Props) => {
 
   const [name, setName] = React.useState('');
-  
-
-
-/******************MAKE SURE USER ENTERS NAME AND NAME IS NOT IN USE******************/
-
 
   return (
-
-    
 
     <View>
       <Portal>
         <Dialog visible={visible} onDismiss={onClose} style={dialogStyles.dialog}>
           <Dialog.Title style={{ color: colours.textPrimary }}>Player Name</Dialog.Title>
           <Dialog.Content>
-            <Text style={{ color:colours.textSecondary }} variant="bodyMedium">Please Enter Your Name:</Text>
-            
+            <Text style={{ color: colours.textSecondary }} variant="bodyMedium">Please Enter Your Name:</Text>
+
             <SafeAreaView>
-            
               <TextInput
                 style={dialogStyles.input}
                 onChangeText={setName}
                 placeholder='My Name'
                 value={name}
               />
-
             </SafeAreaView>
 
           </Dialog.Content>
           <Dialog.Actions>
             <Button onPress={onClose} textColor={colours.textPrimary}>Cancel</Button>
             <Button onPress={() => {
-              if(name.trim().length != 0){
+              if (name.trim().length != 0) {
                 setPlayers(players => [...players, { id: players.length, username: name, score: 0 }]);
                 onClose();
               }
-              else{
-                <Text style={{ fontSize:10, color: colours.textSecondary }}>Please enter a name.</Text>
+              else {
+                Alert.alert('No Username', 'Please enter a name!', [
+                  {
+                    text: 'Okay',
+                    style: 'cancel',
+                  }
+                ]);
               }
               setName('');
             }} textColor={colours.textPrimary}>Add</Button>
           </Dialog.Actions>
-          
+
         </Dialog>
-        
+
       </Portal>
     </View>
 
